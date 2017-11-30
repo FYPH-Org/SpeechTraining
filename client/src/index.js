811b1dfd5ece2928804b27fd63cfc1ed92899744
JCamp-12 <username@users.noreply.github.com>
2017-11-29 19:12:18 -0500

    trying to get routes going

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router , Route , BrowserHistory } from 'react-router-dom';
+import { firebaseApp } from './firebase';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if (user) {
+      console.log('user has signed in or up', user);
+  } else {
+      console.log('user has signed out or still needs to sign in', )
+  } 
+})
+
 
 ReactDOM.render(
-  <Router>  
-    <App />
+  <Router path="/" history={BrowserHistory}>
+    <Route path="/" exact component={App} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/signup" component={SignUp} />
   </Router>, document.getElementById('root'));
-registerServiceWorker();
+
