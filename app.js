28a1af7f32aed870373eb12bba5325bed1d7e91d
jeshsy <jeshsy@users.noreply.github.com>
2017-11-29 21:24:34 -0500

    Database Schema and basic post request

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const dotenv = require('dotenv').config();
 mongoose.Promise = global.Promise;
 
+
+// mongoose stuff
+const User = require('./api/userModel.js');
 // mongoose.connect()
 
 const corsOptions = {
@@ -31,6 +34,51 @@ app.get('/test', (req, res) => {
   res.json({ msg: 'online' });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`server running on port ${process.env.PORT}`);
+
+app.post('/dbtest', (req, res) => {
+  const { Username, email, analysis } = req.body;
+  if (!Username) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Missing required Username' });
+    return;
+  }
+  if (!analysis) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Missing required analysis' });
+    return;
+  }
+  if (!email) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Missing email' });
+    return;
+  }
+  const newUser = new User({ Username, email, analysis });
+  newUser.save((err) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json({ error: 'error' });
+      return;
+    }
+    res.json(newUser);
+  });
+});
+// basic post request
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`server running on port ${process.env.PORT}`);
+// });
+// commented out for mongoose stuff
+mongoose.Promise = global.Promise;
+const connect = mongoose.connect(
+    'mongodb://localhost/User', 
+    { useMongoClient: true }
+);
+
+connect.then(() => {
+    app.listen(process.env.PORT);
+    console.log(`Server listening on port ${process.env.PORT}`);
+}, (err) => {
+    console.log('\n**********************');
+    console.log("ERROR: Failed to connect to MongoDB.");
+    console.log('\n**********************');
 });
