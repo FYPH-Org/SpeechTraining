d72245bfb151368ef5c87eed74fc2ea67d692055
manishavi <manishalal972@gmail.com>
2017-12-13 12:58:51 -0600

    fixing background imaage

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,9 +7,9 @@ class About extends Component {
     return (
       <div>
         <div className='container' >
-          {/*<div class='bg'></div>*/}
+          {/*<div className='bg'></div>*/}
           {/*<img src='speech_img.jpeg' class='img-responsive' />*/}
-          <img src={speechT} alt='speech_img' class='img-responsive'/>
+          <img src={speechT} alt='speech_img' className='img-responsive'/>
           <div className ='well'>Improve Communication Skills with Speech Training</div>
           <br/>
           <div className='panel'>Have confidence and make a good impression whenever you want, and wherever you are with Speech Training.  With the help of the Speech Training app you can learn English in a better and more effective way.  You can enhance your English with correct grammar and positive sentiments. </div>
@@ -18,7 +18,7 @@ class About extends Component {
           <div className='panel'>The Speech Training app converts the speech of the user to text and analyzes the confidence, sentiment, and shows various options to correct the grammatical mistakes.</div>
           <div className='well'>Just follow TWO simple steps: </div>
           <div className='well'>1. Sign in.</div>
-          <div className='well'>1. Start exploring.</div>
+          <div className='well'>2. Start exploring.</div>
           <br />
           <h3>Start Today!</h3>
         </div>
