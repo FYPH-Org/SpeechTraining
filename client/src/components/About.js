dc1ab52d7051d5c958c65a7a9306e5fb51b5c7f7
Ely Alamillo <ely0013@gmail.com>
2017-12-14 14:10:29 -0600

    added styles

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,18 +5,13 @@ import speechT from './speechT.png';
 class About extends Component {
   render() {
     return (
-      <div>
-        <div className='container' >
-          {/*<div className='bg'></div>*/}
-          {/*<img src='speech_img.jpeg' class='img-responsive' />*/}
-          <img src={speechT} alt='speech_img' className='img-responsive'/>
+      <div className='about-container flex-container'>
+        <div className='container'>
           <div className="panel panel-warning">
             <div className="panel-heading">
               <h1 className="panel-title">Improve Communication Skills with Speech Training<a href="#application.urlroot#/html/tags/html_h1_tag.cfm"></a></h1>
             </div>
           </div>
-          {/*<div className ='well'>Improve Communication Skills with Speech Training</div>
-          <br/>*/}
           <div className="panel panel-default">
             <div className="panel-body">Have confidence and make a good impression whenever you want, and wherever you are with Speech Training.  With the help of the Speech Training app you can learn English in a better and more effective way.  You can enhance your English with correct grammar and positive sentiments. </div>
           </div>
@@ -33,7 +28,6 @@ class About extends Component {
               <h1 className="panel-title">Just follow TWO simple steps: <a href="#application.urlroot#/html/tags/html_h1_tag.cfm"></a></h1>
             </div>
           </div>
-          {/*<div className='well'>Just follow TWO simple steps: </div>*/}
           <div className="panel panel-default">
             <div className="panel-body">1. Sign in.<br />2. Start exploring.</div>
           </div>
