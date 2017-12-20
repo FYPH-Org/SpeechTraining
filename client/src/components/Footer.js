07104a90b820e17f7fca85bb2e5524b8f73a0fcf
Ely Alamillo <ely0013@gmail.com>
2017-12-20 15:42:21 -0600

    added footer and styles

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer class="l">
+      <h1> 2017 &#169; <a href="whaatever lik" target="blank"> Speech Trainer</a>
+        &nbsp;| Contact Us </h1>
+    </footer>
+  );
+};
+
+export default Footer;
