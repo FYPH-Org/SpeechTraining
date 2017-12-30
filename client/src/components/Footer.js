4499d8e7ae3e5960756db827832ce80bcbf75c9b
Ely Alamillo <ely0013@gmail.com>
2017-12-29 22:35:05 -0600

    fixed footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,14 +3,10 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className="row">
-      <div className='col-xs-12'>
-        <footer className="custom-footer">
-          <h1> 2017 &#169; <a href="whaatever lik" target="blank"> Speech Trainer</a>
+    <footer className="custom-footer">
+      <h1> 2017 &#169; <a href="whaatever lik" target="blank"> Speech Trainer</a>
         &nbsp;| Contact Us </h1>
-        </footer>
-      </div>
-    </div>
+    </footer>
   );
 };
 
