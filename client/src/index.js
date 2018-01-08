eaafa9a3df9ba51dd50b4c8977d4368a9b416871
Ely Alamillo <ely0013@gmail.com>
2018-01-08 16:03:35 -0600

    formating

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import { firebaseApp } from './firebase';
 import './index.css';
 
